ed2c50275a693729c70955ce76841af228e5bc66
José Fernandes <ze_932@hotmail.com>
2026-06-09 20:30:22 +0100

    remove beta badge

--- src/components/BetaBadge.tsx
+++
@@ -1,7 +0,0 @@
-export function BetaBadge() {
-	return (
-		<span className="inline-flex items-center rounded-full bg-amber-100 px-2 py-0.5 text-xs font-semibold text-amber-700 dark:bg-amber-900/30 dark:text-amber-300 select-none">
-			Beta
-		</span>
-	);
-}
